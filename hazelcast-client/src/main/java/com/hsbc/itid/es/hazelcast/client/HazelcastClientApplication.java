8aaad2d0d3ee0ecf21306bdc28c29567e5deba38
Hins <375838673@qq.com>
2021-03-29 23:00:32 +0800

    update hazelcast client configuration

--- hazelcast-client/src/main/java/com/hsbc/itid/es/hazelcast/client/HazelcastClientApplication.java
+++ hazelcast-client/src/main/java/com/hsbc/itid/es/hazelcast/client/HazelcastClientApplication.java
@@ -1,24 +1,10 @@
 package com.hsbc.itid.es.hazelcast.client;
 
-import com.hazelcast.client.config.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class HazelcastClientApplication {
-
-
-    @Bean
-    public ClientConfig clientConfig() {
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("eureka");
-        clientConfig.getNetworkConfig().getEurekaConfig().setEnabled(true)
-                .setProperty("namespace", "hazelcast");
-        return clientConfig;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HazelcastClientApplication.class, args);
